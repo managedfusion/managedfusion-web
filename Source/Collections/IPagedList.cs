19d58fcd4547e19ffb15ea07564cba717842cc93
Nick Berardi <nick@managedfusion.com>
2010-04-04 22:55:59 -0400

    moved collections over here for paging, and added a custom view to support adding headers to the serialization responses

---
+++ Source/Collections/IPagedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Collections.Generic
+{
+	public interface IPagedList
+	{
+		int TotalCount
+		{
+			get;
+			set;
+		}
+
+		int TotalPages
+		{
+			get;
+			set;
+		}
+
+		int PageIndex
+		{
+			get;
+			set;
+		}
+
+		int PageSize
+		{
+			get;
+			set;
+		}
+
+		bool IsPreviousPage
+		{
+			get;
+		}
+
+		bool IsNextPage
+		{
+			get;
+		}
+	}
+}
---
+++ Source/Collections/PagedList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Collections.Generic
+{
+	public class PagedList<T> : List<T>, IPagedList
+	{
+		public PagedList(IQueryable<T> source, int index, int pageSize)
+		{
+
+			int total = source.Count();
+			this.TotalCount = total;
+			this.TotalPages = total / pageSize;
+
+			if (total % pageSize > 0)
+				TotalPages++;
+
+			this.PageSize = pageSize;
+			this.PageIndex = index;
+			this.AddRange(source.Skip(index * pageSize).Take(pageSize).ToList());
+		}
+
+		public PagedList(List<T> source, int index, int pageSize)
+		{
+
+			int total = source.Count();
+			this.TotalCount = total;
+			this.TotalPages = total / pageSize;
+
+			if (total % pageSize > 0)
+				TotalPages++;
+
+			this.PageSize = pageSize;
+			this.PageIndex = index;
+			this.AddRange(source.Skip(index * pageSize).Take(pageSize).ToList());
+		}
+
+		public int TotalPages
+		{
+			get;
+			set;
+		}
+
+		public int TotalCount
+		{
+			get;
+			set;
+		}
+
+		public int PageIndex
+		{
+			get;
+			set;
+		}
+
+		public int PageSize
+		{
+			get;
+			set;
+		}
+
+		public bool IsPreviousPage
+		{
+			get
+			{
+				return (PageIndex > 0);
+			}
+		}
+
+		public bool IsNextPage
+		{
+			get
+			{
+				return (PageIndex * PageSize) <= TotalCount;
+			}
+		}
+	}
+}
